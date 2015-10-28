5ed53d33a9073f30897b253846d4e2e61d8b81f3
George Ascherl <georgea@cox.net>
2015-10-28 11:16:31 -0700

    second commit

---
+++ flow_control.js
@@ -0,0 +1,18 @@
+// Basic flow control
+// IF / ELSE IF / ELSE
+// WHILE
+// DO WHILE
+
+if (condition) {
+  // Do some code
+} else if (condition2) {
+  // Do some other code
+} else if (condition3) {
+  // Do some code
+} else {
+  // Do some other code
+}
+
+while (condition) {
+  //do some code
+}
