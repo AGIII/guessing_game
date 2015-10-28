83a7be8444b8dd4648bb9f358932e1c7f1d929e7
George Ascherl <georgea@cox.net>
2015-10-28 10:47:25 -0700

    first commit

---
+++ game.js
@@ -0,0 +1,51 @@
+    var tally = 0;
+    alert('Get ready to play a guessing game');
+
+    var name = prompt('What is my middle name?');
+
+  if (name.toLowerCase() === 'francis') {
+    tally = tally + 1;
+    alert('Correct!' + ' ' + 'You answered ' + name + '.');
+    console.log('The user answered correctly with: ' + name);
+  } else {
+    alert('Incorrect!' + ' ' + 'You answered ' + name + '.');
+    console.log('The user answered incorrectly with: ' + name);
+  }
+
+    var from = prompt('Where am I from?');
+
+  if (from.toLowerCase() === 'los angeles') {
+    tally = tally + 1;
+    alert('Correct!' + ' ' + 'You answered ' + from + '.');
+    console.log('The user answered correctly with: ' + from);
+  } else {
+    alert('Incorrect!' + ' ' + 'You answered ' + from + '.');
+    console.log('The user answered incorrectly with: ' + from);
+  }
+
+  var born = prompt('What year was I born?');
+
+  if (parseInt(born) === 1986) {
+    tally = tally + 1;
+    alert('Correct!' + ' ' + 'You answered ' + born + '.');
+    console.log('The user answered correctly with: ' + born);
+  } else {
+    alert('Incorrect!' + ' ' + 'You answered ' + born + '.');
+    console.log('The user answered incorrectly with: ' + born);
+  }
+
+  var temp = prompt('What is the temperature right now in degrees fahrenheit?');
+
+  if (parseInt(temp) === 55) {
+    tally = tally + 1;
+    alert('Correct! It is' + temp + ' degrees fahrenheit outside.');
+    console.log('The user answered correctly with: ' + temp + 'degrees fahrenheit.');
+  } else if (parseInt(temp) > 55.001) {
+    alert('Incorrect! You answered ' + temp + ' degrees fahrenheit, which is too high.');
+    console.log('The user answered ' + temp + ' degrees fahrenheit, which is too high.');
+  } else if (parseInt(temp) < 49.999) {
+    alert('Incorrect! You answered ' + temp + ' degrees fahrenheit, which is too low.');
+    console.log('The user answered ' + temp + ' degrees fahrenheit, which is too low.');
+  }
+
+  alert('You got ' + tally + ' out of 4 correct!');
